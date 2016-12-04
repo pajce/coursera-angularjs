8de995cc7ff1aba1820f50a7acb8a4aa9e3352c7
Petar Pajic <ppajce@gmail.com>
2016-12-04 23:20:57 +0100

    Update app.js

--- mod2_solution/app.js
+++ mod2_solution/app.js
@@ -32,13 +32,10 @@ function ShoppingListCheckOffService() {
   service.itemsToBuy = [
     { name: "Chips", quantity: "10 bags" },
     { name: "Coca Cola", quantity: "5 bottles"},
-    { name: "Chips", quantity: "7 bags" },
-    { name: "Popcorn", quantity: "10 bags" },
-    { name: "Pizza", quantity: "5 rings" },
+    { name: "Popcorn", quantity: "11 bags" },
     { name: "Cookies", quantity: "15 pieces"  },
     { name: "Chocolate", quantity: "3 tables" },
-    { name: "Pepsi", quantity: "3 bottles" },
-    { name: "Beer", quantity: "50 large bottles" }
+    { name: "Pepsi", quantity: "3 bottles" }
   ] ;
 
   service.boughtItems =[ ];
